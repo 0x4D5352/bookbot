d9b70ecc5bcd2071401afc5d67391093331372eb
Joshua Nussbaum <67082011+0x4D5352@users.noreply.github.com>
2024-05-21 22:19:08 -0500

    add broken count list

--- main.py
+++ main.py
@@ -17,11 +17,22 @@ def count_words(book):
     return len(book.split())
 
 
+def sort_counts(letters):
+    letters_list = []
+    for letter in letters:
+        letters_list.append({"letter": letter, "count": letters[letter]})
+    sort_on_count = lambda x: x["count"]
+    return letters_list.sort(reverse=True, key=sort_on_count)
+
+
 def main():
     title = "frankenstein.txt"
     book = read_book(title)
-    print(f"{title} contains {count_words(book)} words!")
-    print(count_letters(book))
+    print(f"--- begin report of {title} ---")
+    print(f"{title} contains {count_words(book)} words!\n")
+    for letter in sort_counts(count_letters(book)):
+        print(f"the {letter["letter"]} character was found {letter["count"]} times")
+    print("--- End report ---")
 
 
 if __name__ == "__main__":
